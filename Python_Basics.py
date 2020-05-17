46d31cd25bb364826eea8b5fea173b459514dc51
Udit Kumar Chatterjee <quantumudit@gmail.com>
2020-05-18 02:55:46 +0530

    Adds notes on Python Basics

---
+++ Python_Basics.py
@@ -0,0 +1,41 @@
+# To Print anything on python :
+
+print("Hello World !!")
+
+# Simple Arithmetical Operations
+
+#Addition
+
+print(2+3)
+
+#Subtraction
+print(5-3)
+
+#Multiplication
+print(5*5)
+
+#Division
+print(10/5)
+
+#Exponent
+print(2**3)
+
+'''
+Problem-1 :
+Let the price of a doll is $100 and its price incereases by $10 each year,
+then, what would be its price after 5 years ?
+'''
+
+#Solution-1
+print(100 + 10 * 5)
+
+
+# Introducing Variables
+# Store values of problem-1 in variables and then, printing the result
+
+doll_price = 100
+increment = 10
+years = 5
+
+future_price = doll_price + increment * years
+print(future_price)
