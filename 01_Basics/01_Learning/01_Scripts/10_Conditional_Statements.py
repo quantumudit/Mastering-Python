a9a9c1b06a4711ea9c848452693f6c7f323276e2
Udit Kumar Chatterjee <quantumudit@gmail.com>
2021-05-02 02:52:23 +0530

    Basics of conditional execution

---
+++ 01_Basics/01_Learning/01_Scripts/10_Conditional_Statements.py
@@ -0,0 +1,26 @@
+# Conditional Executions #
+# ====================== #
+
+check = False
+
+if check == False:
+    print("It is false")
+
+# if-else statement
+
+if check == False:
+    print("It is false")
+else:
+    print("It is actually equal to True")
+
+
+# elif statements
+
+if check == False:
+    print("It is false")
+elif check == "Hamburger":
+    print("Yummy")
+elif check == "Yo":
+    print("Hello !")
+else:
+    print("Thats a tidious process, overall")
