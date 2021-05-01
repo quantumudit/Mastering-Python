b7fe26c0d2ea4f85df2e735fab2ae8879887c4ea
Udit Kumar Chatterjee <quantumudit@gmail.com>
2021-05-02 02:52:38 +0530

    Uttilizing the UDFs

--- 01_Basics/01_Learning/01_Scripts/08_User_Defined_Functions.py
+++ 01_Basics/01_Learning/01_Scripts/08_User_Defined_Functions.py
@@ -16,4 +16,25 @@ def StringConcat(str1, str2):
 
 StringConcat("Apple", "Banana")
 
-# Function
+# Function with default values
+
+
+def DescribePerson(name="YOUR NAME", age="YOUR AGE"):
+    print("My name is ", name, "and my age is ", age)
+
+
+DescribePerson("Udit", 26)
+
+# Calling just the age with keyword argument
+
+DescribePerson(age=35)
+
+# Infinite number of arguments in a function
+
+
+def PrintPeople(*people):
+    for person in people:
+        print("This person is ", person)
+
+
+PrintPeople("Nick", "Jack", "Solomon", "Darwin", "Gordon")
