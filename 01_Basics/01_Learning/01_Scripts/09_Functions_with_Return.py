bdcb398f8bed5b4cc9310822848ede76992ad9e7
Udit Kumar Chatterjee <quantumudit@gmail.com>
2021-05-02 02:51:52 +0530

    Functions with Return values

---
+++ 01_Basics/01_Learning/01_Scripts/09_Functions_with_Return.py
@@ -0,0 +1,11 @@
+# Functions with Return Statement #
+# ============================== #
+
+def DoMath(num1, num2):
+    return num1 + num2
+
+
+math1 = DoMath(5, 7)
+math2 = DoMath(8, 9)
+
+print("The first sum is ", math1, "\n The second sum is ", math2)
